0a6223564bac50395bc67a032d9d755de26f4a60
BlankDev117 <mmmasent@gmail.com>
2026-04-15 17:02:51 -0400

    Add Error output extension for multioutput (OSK-3) (#4)
    Motivation
    ----
    A multioutput that returns a multistatus indicates a plurality of
    statuses, which could include one or more error outputs. In this case,
    it'd be nice to know if any error outputs are present on the multistatus
    
    Modifications
    ----
    * Added `MultiOutputExtensions` to provide a quick helper to get error
    related status outputs on a multioutput, if any are present
    
    Result
    ----
    MultiOutputs have an easy to use extension to get error outputs
    
    Fixes #3

---
+++ src/OSK.Operations.Outputs/MultiOutputExtensions.cs
@@ -0,0 +1,16 @@
+﻿using OSK.Operations.Outputs.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OSK.Operations.Outputs;
+
+public static class MultiOutputExtensions
+{
+    /// <summary>
+    /// Returns outputs from the output collection that are associated to error statuses
+    /// </summary>
+    /// <param name="output">The <see cref="MultiOutput"/> to get outputs from</param>
+    /// <returns>The collection of error outputs</returns>
+    public static IEnumerable<Output> GetErrorOutputs(this MultiOutput output)
+        => output.Data.Where(o => !o.IsSuccessful);
+}
